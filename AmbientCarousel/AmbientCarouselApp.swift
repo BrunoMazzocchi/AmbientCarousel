b4d802026567afcf8c5dba308d322cc18aa7b70a
Bruno Mazzocchi <bmazzocchi@tribalworldwide.gt>
2025-01-04 09:21:57 -0600

    Initial Commit

---
+++ AmbientCarousel/AmbientCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientCarouselApp.swift
+//  AmbientCarousel
+//
+//  Created by Bruno Mazzocchi on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AmbientCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
